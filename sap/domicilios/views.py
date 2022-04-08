6926f0c77656d6b6b7f983babc2e4f216e94500c
Albert Castillo <albert.castillo.oses@gmail.com>
2022-04-08 14:50:19 -0300

    Terminado editar y eliminar domicilio

--- sap/domicilios/views.py
+++ sap/domicilios/views.py
@@ -23,3 +23,22 @@ def agregarDomicilio(request):
     else:
         formaDomicilio = DomicilioForm()
     return render(request, 'domicilio/agregar.html', {'fDomicilio': formaDomicilio})
+
+
+def editarDomicilio(request, id):
+    editDomicilio = get_object_or_404(Domicilio, pk=id)
+    if request.method == 'POST':
+        formaDomicilio = DomicilioForm(request.POST, instance=editDomicilio)
+        if formaDomicilio.is_valid():
+            formaDomicilio.save()
+            return redirect('index2')
+    else:
+        formaDomicilio = DomicilioForm(instance=editDomicilio)
+    return render(request, 'domicilio/editar.html', {'fDomicilio': formaDomicilio})
+
+
+def eliminarDomicilio(request, id):
+    elimDomicilio = get_object_or_404(Domicilio, pk=id)
+    if elimDomicilio:
+        elimDomicilio.delete()
+    return redirect('index2')
