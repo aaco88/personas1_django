ea81565970caf478f197c242e8766fa5a7cf1372
Albert Castillo <albert.castillo.oses@gmail.com>
2022-04-07 22:45:16 -0300

    Primera vista pagina de inicio

--- sap/webapp/views.py
+++ sap/webapp/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def bienvenido(request):
+    return render(request, 'bienvenido.html', {'msg1': 'Mensaje1', 'msg2': 'Mensaje2'})
